0a3848771236e9ce68422bf2524aaf43b6a7412e
Illaf <114517448+Illaf@users.noreply.github.com>
2023-02-21 22:54:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 import './App.scss';
-import {Route, Routes } from 'react-router-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 import Layout from './component/Layout'
 import Home from './component/Home'
